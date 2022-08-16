baa21c132b856357e2f699bf9c8d2ae34f0953f5
tommytommy-ヒーロー <109708488+tommytommy-hero@users.noreply.github.com>
2022-08-16 23:21:42 +0000

    [Start]ハンバーガー

---
+++ SlideShow-Lesson/js/script.js
@@ -0,0 +1,11 @@
+$(".theTarget").skippr({
+  transition : 'fade',
+  speed : 1000,
+  easing : 'block',
+  childrenElementType : 'div',
+  arrows : true,
+  autoPlay : false,
+  autoPlayDuration :5000,
+  keyboardOnAlways : true,
+  hidePrevious : false
+});
